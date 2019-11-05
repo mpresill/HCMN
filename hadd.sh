884bb8972b1a0d5aab7fa97b4d5837eb9a9baefe
mpresill <26869915+mpresill@users.noreply.github.com>
2019-11-05 10:50:16 +0100

    Update hadd.sh

--- hadd.sh
+++ hadd.sh
@@ -1,11 +1,11 @@
 rm hadd.txt
 rm tmp.root
 
-echo "hadd -f WW_2016.root" > hadd.txt
+echo "hadd -f WW_2016.root" > hadd.txt #EDIT: change name of the final root file
 
 t=0
 
-for i in `ls -altr WW_ | grep root | awk '{print $9}'`;
+for i in `ls -altr WW_ | grep root | awk '{print $9}'`; #EDIT: name of the task (the "hadd.sh" needs to stay in the output folder)
 do
 
 echo "`cat hadd.txt` WW_/$i" > hadd.txt
