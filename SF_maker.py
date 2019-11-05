220dc2bf3147d5ce45d2f9bf725fb805bd6bce65
mpresill <26869915+mpresill@users.noreply.github.com>
2019-11-05 15:54:17 +0100

    adapted for 2017 jsons

--- SF_maker.py
+++ SF_maker.py
@@ -2,35 +2,76 @@ import json
 import ROOT
 import numpy as np
 
-with open("RunBCDEF_SF_TRIG.json") as f1:
+with open("RunBCDEF_SF_ID.json") as f1:
     test = json.load(f1)
 
-with open("RunGH_SF_TRIG.json") as f2:
-    test2 = json.load(f2)
 
-print ("pt_low;pt_high;eta_low;eta_high;value;stat;syst")
-for key, value in test["Mu50_OR_TkMu50_PtEtaBins"]["pt_abseta_ratio"].items():
+print ("eta_low;eta_high;pt_low;pt_high;value;stat;syst")
+for key, value in test["NUM_LooseID_DEN_genTracks"]["abseta_pt"].items():
 
-    pt = key.split(":")[1]
-    pt_low = pt.split("[")[1].split(',')[0]
-    pt_high = pt.split("]")[0].split(',')[1]
+    eta = key.split(":")[1]
+    eta_low = eta.split("[")[1].split(',')[0]
+    eta_high = eta.split("]")[0].split(',')[1]
 
-    print("if(pt>%s && pt<=%s){" % (pt_low, pt_high))
-    
-    
-    
-    for eta_int, dic in value.items():
+    print("if(fabs(eta)>%s && fabs(eta)<=%s){" % (eta_low, eta_high))
 
-        dic2 = test2["Mu50_OR_TkMu50_PtEtaBins"]["pt_abseta_ratio"][key][eta_int]
 
-        eta = eta_int.split(":")[1]
-        eta_low = eta.split("[")[1].split(",")[0]
-        eta_high = eta.split("]")[0].split(",")[1]
+    for pt_int, dic in value.items():
 
-        value = (dic["value"]*35.89+dic2["value"]*32.58)/68.47
-        error = (dic["error"]*35.89+dic2["error"]*32.58)/68.47
-       # syst = (dic["syst"]*35.89+dic2["syst"]*32.58)/68.47
+        pt = pt_int.split(":")[1]
+        pt_low = pt.split("[")[1].split(",")[0]
+        pt_high = pt.split("]")[0].split(",")[1]
 
-        print("    if(eta > %s && eta <= %s) musf=%s; // error %s " % (eta_low, eta_high, value, error))
+        value = dic["value"]
+        error = dic["error"]
+      # syst = dic["syst"]
+
+        print("    if(pt > %s && pt <= %s) musf=%s; // error %s " % (pt_low, pt_high, value, error))
 
     print("}")
+                                    
+
+
+#import json
+#import ROOT
+#import numpy as np
+
+#with open("RunBCDEF_SF_TRIG.json") as f1:
+#    test = json.load(f1)
+
+#with open("RunGH_SF_TRIG.json") as f2:
+#    test2 = json.load(f2)
+
+#print ("pt_low;pt_high;eta_low;eta_high;value;stat;syst")
+#for key, value in test["Mu50_OR_TkMu50_PtEtaBins"]["pt_abseta_ratio"].items():
+
+ #   pt = key.split(":")[1]
+#    pt_low = pt.split("[")[1].split(',')[0]
+#    pt_high = pt.split("]")[0].split(',')[1]
+
+ #   print("if(pt>%s && pt<=%s){" % (pt_low, pt_high))
+    
+    
+    
+  #  for eta_int, dic in value.items():
+
+   #     dic2 = test2["Mu50_OR_TkMu50_PtEtaBins"]["pt_abseta_ratio"][key][eta_int]
+
+    #    eta = eta_int.split(":")[1]
+      #  eta_low = eta.split("[")[1].split(",")[0]
+     #   eta_high = eta.split("]")[0].split(",")[1]
+
+      #  value = (dic["value"]*35.89+dic2["value"]*32.58)/68.47
+      #  error = (dic["error"]*35.89+dic2["error"]*32.58)/68.47
+       # syst = (dic["syst"]*35.89+dic2["syst"]*32.58)/68.47
+
+       # print("    if(eta > %s && eta <= %s) musf=%s; // error %s " % (eta_low, eta_high, value, error))
+
+#    print("}")
+
+    
+    
+   
+    
+    
+    
