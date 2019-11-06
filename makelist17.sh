14255cd75fef7a33d9cd91335ae5cf08518d5869
mpresill <26869915+mpresill@users.noreply.github.com>
2019-11-06 16:37:44 +0100

    Update makelist17.sh

--- makelist17.sh
+++ makelist17.sh
@@ -62,8 +62,62 @@ do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> listWW.txt; done
 
 sed -i -e "s/\"/\\\\\"/g" listWW.txt
 sed -i -e "s/\//\\\\\//g" listWW.txt
- 
 
+#DY
+for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/Fall17V2_DY1/191023_102411/0000`;
+do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> listDY.txt
+
+for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/Fall17V2_DY2/191023_102649/0000`;
+do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> listDY.txt
+
+sed -i -e "s/\"/\\\\\"/g" listDY.txt
+sed -i -e "s/\//\\\\\//g" listDY.txt
+
+#TT
+for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/Fall17V2_TT/191022_133904/0000`;
+do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> listTT.txt; done
+
+for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/Fall17V2_TT/191023_094710/0000`;
+do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> listTT.txt; done
+
+sed -i -e "s/\"/\\\\\"/g" listTT.txt
+sed -i -e "s/\//\\\\\//g" listTT.txt
+
+#mumujj_L13_M500
+for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/HavyCompositeMajoranaNeutrino_L13000_M500_mumujj_CalcHep/mumujj_17_L13_M500/191023_104143/0000`;
+do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> list_mumujjL13M500.txt; done
+
+sed -i -e "s/\"/\\\\\"/g" list_mumujjL13M500.txt
+sed -i -e "s/\//\\\\\//g" list_mumujjL13M500.txt
+
+#mumujj_L13_M1000
+for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/HavyCompositeMajoranaNeutrino_L13000_M1000_mumujj_CalcHep/mumujj_17_L13_M1000/191023_104415/0000`;
+do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> list_mumujjL13M1000.txt; done
+
+sed -i -e "s/\"/\\\\\"/g" list_mumujjL13M1000.txt
+sed -i -e "s/\//\\\\\//g" list_mumujjL13M1000.txt
+
+#mumujj_L13_M2000
+for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/HavyCompositeMajoranaNeutrino_L13000_M2000_mumujj_CalcHep/mumujj_17_L13_M2000/191023_104644/0000`;
+do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> list_mumujjL13M2000.txt; done
+
+sed -i -e "s/\"/\\\\\"/g" list_mumujjL13M2000.txt
+sed -i -e "s/\//\\\\\//g" list_mumujjL13M2000.txt
+
+#mumujj_L13_M5000
+for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/HavyCompositeMajoranaNeutrino_L13000_M5000_mumujj_CalcHep/mumujj_17_L13_M5000/191023_104909/0000`;
+do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> list_mumujjL13M5000.txt; done
+
+sed -i -e "s/\"/\\\\\"/g" list_mumujjL13M5000.txt
+sed -i -e "s/\//\\\\\//g" list_mumujjL13M5000.txt
+
+#mumujj_L13_M8000
+for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/HavyCompositeMajoranaNeutrino_L13000_M8000_mumujj_CalcHep/mumujj_17_L13_M8000/191023_105135/0000`;
+do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> list_mumujjL13M8000.txt; done
+
+sed -i -e "s/\"/\\\\\"/g" list_mumujjL13M8000.txt
+sed -i -e "s/\//\\\\\//g" list_mumujjL13M8000.txt
+ 
 #WJets
 #100200
 for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8/Fall17V2_WJetsHT100To200/191023_100211/0000`;
@@ -103,25 +157,6 @@ do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> listWJets.txt; done
 sed -i -e "s/\"/\\\\\"/g" listWJets.txt
 sed -i -e "s/\//\\\\\//g" listWJets.txt
 
-#DY
-for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/Fall17V2_DY1/191023_102411/0000`;
-do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> listDY.txt
-
-for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/Fall17V2_DY2/191023_102649/0000`;
-do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> listDY.txt
-
-sed -i -e "s/\"/\\\\\"/g" listDY.txt
-sed -i -e "s/\//\\\\\//g" listDY.txt
-
-#TT
-for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/Fall17V2_TT/191022_133904/0000`;
-do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> listTT.txt; done
-
-for i in `xrdfs root://t2-xrdcms.lnl.infn.it:7070 ls /store/user/mpresill/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/Fall17V2_TT/191023_094710/0000`;
-do echo "root://t2-xrdcms.lnl.infn.it:7070/${i//\/cms/}" >> listTT.txt; done
-
-sed -i -e "s/\"/\\\\\"/g" listTT.txt
-sed -i -e "s/\//\\\\\//g" listTT.txt
 
 #Signals
 #eejj_L13_M500
