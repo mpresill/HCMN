c1331f8cb585976f7b960bdacf913e4340c81b82
matteo <matteo.presilla@pg.infn.it>
2020-01-07 11:11:31 +0100

    Sr update

--- BkgEstimation/SkimNtuple.cc
+++ BkgEstimation/SkimNtuple.cc
@@ -30,23 +30,24 @@ Need to specify
 //   Declare constants
 /////
 //Path - samples 
-const string path     = "/eos/user/m/mpresill/CMS/HN_Reload/rootplized_samples_2017/"; ///eos/user/v/vmariani/NTuples/HN_2016/ //  /eos/user/v/vmariani/NTuples/HN_2018/
-const char *samples[] = {"TT_2017","tW_2017","Other_2017","DY_2017","TTtW_2017",
-                         //"data_ele_2017",
-                         "data_mu_2017",
-                         "mumujj_L13000_M500_2017","mumujj_L13000_M1000_2017","mumujj_L13000_M2000_2017","mumujj_L13000_M5000_2017","mumujj_L13000_M8000_2017"
-			/*,"eejj_L13000_M500_2017","eejj_L13000_M1000_2017","eejj_L13000_M2000_2017","eejj_L13000_M5000_2017","eejj_L13000_M8000_2017"*/                                         };
+const string path     = "/eos/user/v/vmariani/NTuples/HN_2016/";   //"/eos/user/m/mpresill/CMS/HN_Reload/rootplized_samples_2017/"; // /eos/user/m/mpresill/CMS/HN_Reload/rootplized_samples_2017/ // /eos/user/v/vmariani/NTuples/HN_2016/ // /eos/user/v/vmariani/NTuples/HN_2018/
+const char *samples[] = {/*"TT_2016","tW_2016",*/ "Other_2016","DY_2016","TTtW_2016",
+                         "data_ele_2016"
+                         //"data_mu_2016"
+                         //"mumujj_L13000_M500_2016","mumujj_L13000_M1000_2016","mumujj_L13000_M2000_2016","mumujj_L13000_M5000_2016","mumujj_L13000_M8000_2016"
+			"eejj_L13000_M500_2016","eejj_L13000_M1000_2016","eejj_L13000_M2000_2016","eejj_L13000_M5000_2016","eejj_L13000_M8000_2016" 
+			};
 const string specsel  = "";
 //Selection
 const string suffisso   = 
                           ///////muon/////////////////////
-                          "SRmu";
+                          //"SRmu";
                           //"DYRmu";
                           //"DYRmuPlot";
                           //"DYRmuTest";
                           //"TRmu";   //CR region TTtW estimation: the second leading lepton has opposite flavor
                           ////////ele/////////////////
-                          //"SRe";
+                          "SRe";
                           //"DYRe";
                           //"DYRePlot";
                           //"DYReTest";
@@ -54,17 +55,17 @@ const string suffisso   =
                      
 const string selection  =
 ////////////////////////muon/////////////////////////////
-"HLT_Mu50==1 && numOfHighptMu==2 && Muon_pt[0]>250 && Muon_pt[1]>120 && numOfVetoEle==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>500";
-//"HLT_Mu50==1 && numOfHighptMu==2 && Muon_pt[0]>250 && Muon_pt[1]>120 && numOfVetoEle==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>80 && M_leplep<100";
-//"HLT_Mu50==1 && numOfHighptMu==2 && Muon_pt[0]>250 && Muon_pt[1]>120 && numOfVetoEle==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>50 && M_leplep<130";
-//"HLT_Mu50==1 && numOfHighptMu==2 && Muon_pt[0]>250 && Muon_pt[1]>120 && numOfVetoEle==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>80 && M_leplep<500";
-//"HLT_Mu50==1 && numOfHighptMu==1 &&  Muon_pt[0]>250 && numOfHighptEle==1 && patElectron_pt[0]>120 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>500";
+//"HLT_Mu50==1 && numOfHighptMu==2 && Muon_pt[0]>250 && Muon_pt[1]>100 && numOfVetoEle==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>450";
+//"HLT_Mu50==1 && numOfHighptMu==2 && Muon_pt[0]>250 && Muon_pt[1]>100 && numOfVetoEle==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>80 && M_leplep<100";
+//"HLT_Mu50==1 && numOfHighptMu==2 && Muon_pt[0]>250 && Muon_pt[1]>100 && numOfVetoEle==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>50 && M_leplep<130";
+//"HLT_Mu50==1 && numOfHighptMu==2 && Muon_pt[0]>250 && Muon_pt[1]>100 && numOfVetoEle==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>80 && M_leplep<450";
+//"HLT_Mu50==1 && numOfHighptMu==1 &&  Muon_pt[0]>250 && numOfHighptEle==1 && patElectron_pt[0]>100 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>450";
 ////////////////////ele////////////////////////////////////
-//"HLT_Ele115_CaloIdVT_GsfTrkIdT==1 && numOfHighptEle==2 && patElectron_pt[0]>250 && patElectron_pt[1]>120 && numOfLooseMu==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>300 && M_leplep>500";
-//"HLT_Ele115_CaloIdVT_GsfTrkIdT==1 && numOfHighptEle==2 && patElectron_pt[0]>250 && patElectron_pt[1]>120 && numOfLooseMu==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>80 && M_leplep<100";
-//"HLT_Ele115_CaloIdVT_GsfTrkIdT==1 && numOfHighptEle==2 && patElectron_pt[0]>250 && patElectron_pt[1]>120 && numOfLooseMu==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>50 && M_leplep<530";
-//"HLT_Ele115_CaloIdVT_GsfTrkIdT==1 && numOfHighptEle==2 && patElectron_pt[0]>250 && patElectron_pt[1]>120 && numOfLooseMu==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>80 && M_leplep<500";
-//"HLT_Ele115_CaloIdVT_GsfTrkIdT==1 && numOfHighptEle==1 && patElectron_pt[0]>250 && numOfHighptMu==1 &&  Muon_pt[0]>120 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>500";
+//"HLT_Ele115_CaloIdVT_GsfTrkIdT==1 && numOfHighptEle==2 && patElectron_pt[0]>250 && patElectron_pt[1]>100 && numOfLooseMu==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>450";
+//"HLT_Ele115_CaloIdVT_GsfTrkIdT==1 && numOfHighptEle==2 && patElectron_pt[0]>250 && patElectron_pt[1]>100 && numOfLooseMu==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>80 && M_leplep<100";
+//"HLT_Ele115_CaloIdVT_GsfTrkIdT==1 && numOfHighptEle==2 && patElectron_pt[0]>250 && patElectron_pt[1]>100 && numOfLooseMu==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>50 && M_leplep<450";
+//"HLT_Ele115_CaloIdVT_GsfTrkIdT==1 && numOfHighptEle==2 && patElectron_pt[0]>250 && patElectron_pt[1]>100 && numOfLooseMu==0 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>80 && M_leplep<450";
+//"HLT_Ele115_CaloIdVT_GsfTrkIdT==1 && numOfHighptEle==1 && patElectron_pt[0]>250 && numOfHighptMu==1 &&  Muon_pt[0]>100 && numOfBoostedJets>=1 && BoostedJet_pt[0]>190 && M_leplep>450";
 const string dotroot    = ".root"; 
 /////
 //   Declare functions 
