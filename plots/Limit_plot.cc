c2f3f38f6659d285fd523204b5e61ce33e1f5e35
Matteo Presilla <matteo.presilla@cern.ch>
2020-05-02 12:50:25 +0200

    a

--- plots/Limit_plot.cc
+++ plots/Limit_plot.cc
@@ -42,8 +42,7 @@ const bool comb        = false;
 const bool thcorrected = false;
 const bool   plot_r    = false; //r is the strenght; the alternative is to plot xsec times Br
 const bool   save_plot = true;
-if(eejj){ const string name_plot = "eejj_L13000_stat_only_2016-2May20.pdf";} //xsecBR or r}
-if(mumujj){ const string name_plot = "mumujj_L13000_stat_only_2016-2May20.pdf";} //xsecBR or r}}
+const string name_plot = "eejj_L13000_stat_only_2016-2May20.pdf";
 const int    val       = 5;
 
 //limits from Combine
