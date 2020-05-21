a6e63ee73c027f24310ee524dc80d70cf8282eb2
Matteo Presilla <matteo.presilla@cern.ch>
2020-05-21 13:08:33 +0200

    update

--- Rootplizer_HeavyNeutrino_syst_2016.cc
+++ Rootplizer_HeavyNeutrino_syst_2016.cc
@@ -1045,8 +1045,9 @@ void  filename_(const char*  Input = "", const char*  Output =""){
       //////////////////////////////////////////////////
       //////////////////////////////////////////////////
       //systematics JES UP TEST
-      double jet_pt_JesSFup=rBoostedJet_Uncorr_pt->at(jet_en) * rBoostedJet_JesSFup->at(jet_en);
-      BoostedJet_JesSFup_pt->push_back( rBoostedJet_Uncorr_pt->at(jet_en) * rBoostedJet_JesSFup->at(jet_en) );
+      cout<<"jetcorr size "<<JetCorr.Pt()->size()<<endl;
+      cout<<"bj sf jes up" <<BoostedJet_JesSFup_pt->size()<<endl;
+      BoostedJet_JesSFup_pt->push_back( JetCorr.Pt() );//rBoostedJet_Uncorr_pt->at(jet_en) * rBoostedJet_JesSFup->at(jet_en) );
       //////////////////////////////////////////////////      
       //////////////////////////////////////////////////
      }
@@ -1173,7 +1174,7 @@ void  filename_(const char*  Input = "", const char*  Output =""){
       //////////////////////////////////////////////////
       //////////////////////////////////////////////////
       //systematics JES UP TEST
-      if(BoostedJet_JesSFup_pt->size()>0){
+      /*if(BoostedJet_JesSFup_pt->size()>0){
         TLorentzVector BoostedJet1_JesSFup(0,0,0,0); BoostedJet1_JesSFup.SetPtEtaPhiE(BoostedJet_JesSFup_pt->at(0), BoostedJet_eta->at(0), BoostedJet_phi->at(0), BoostedJet_energy->at(0));
         if(eejj_l==1){
            M_leplepBjet_JesSFup=(Ele1 + Ele2 + BoostedJet1_JesSFup ).M();
@@ -1184,7 +1185,7 @@ void  filename_(const char*  Input = "", const char*  Output =""){
         if(emujj_l==1 || muejj_l==1){
           M_leplepBjet_JesSFup=(Ele1 + Ele2 + BoostedJet1_JesSFup ).M();
         }
-     }
+     }*/
      //////////////////////////////////////////////////
      //////////////////////////////////////////////////
 
