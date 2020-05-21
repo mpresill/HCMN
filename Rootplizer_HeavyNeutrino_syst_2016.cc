6bb48d12fa2c9e712caeb85f10741b1163a22391
Matteo Presilla <matteo.presilla@cern.ch>
2020-05-21 11:29:54 +0200

    s

--- Rootplizer_HeavyNeutrino_syst_2016.cc
+++ Rootplizer_HeavyNeutrino_syst_2016.cc
@@ -331,7 +331,7 @@ void  filename_(const char*  Input = "", const char*  Output =""){
 
 ///////////////////////////////////////
 ///////////systematics JesSFup
-   vector<double>* BoostedJet_JesSFup_pt = new std::vector<double>; newtree->Branch("BoostedJet_JesSFup_pt",&BoostedJet_JesSFup_pt);
+   vector<double>* BoostedJet_JesSFup_pt = new std::vector<double>; //newtree->Branch("BoostedJet_JesSFup_pt",&BoostedJet_JesSFup_pt);
 //////////////////////////////////////
 
 
