4cfa0ba530ba18bd774ea0d09113ce0899f437a7
Matteo Presilla <matteo.presilla@cern.ch>
2020-06-04 21:31:33 +0200

    fix =

--- Rootplizer_HeavyNeutrino_syst_2016_DEV.cc
+++ Rootplizer_HeavyNeutrino_syst_2016_DEV.cc
@@ -931,21 +931,21 @@ void  filename_(const char*  Input = "", const char*  Output =""){
   ///////corrections on boosted jets with systematics up and down
   /////////////////////////////////////////////////
 
-      if(BJETSF=0){      //BJet corrections with central values of JER/JES:
+      if(BJETSF==0){      //BJet corrections with central values of JER/JES:
           double jet_pt = rBoostedJet_Uncorr_pt->at(jet_en)*rBoostedJet_JesSF->at(jet_en);
           double jet_energy=rBoostedJet_energy->at(jet_en)*rBoostedJet_Uncorr_pt->at(jet_en)/rBoostedJet_pt->at(jet_en)*rBoostedJet_JesSF->at(jet_en);
           TLorentzVector JetCorr(0,0,0,0); JetCorr.SetPtEtaPhiE(jet_pt, rBoostedJet_eta->at(jet_en), rBoostedJet_phi->at(jet_en), jet_energy);
           //// 
           centralJesJer = 1;
         }
-        if(BJETSF=1){      //BJet corrections with JES SF UP:
+        if(BJETSF==1){      //BJet corrections with JES SF UP:
           double jet_pt = rBoostedJet_Uncorr_pt->at(jet_en)*rBoostedJet_JesSFup->at(jet_en);
           double jet_energy=rBoostedJet_energy->at(jet_en)*rBoostedJet_Uncorr_pt->at(jet_en)/rBoostedJet_pt->at(jet_en)*rBoostedJet_JesSFup->at(jet_en);
           TLorentzVector JetCorr(0,0,0,0); JetCorr.SetPtEtaPhiE(jet_pt, rBoostedJet_eta->at(jet_en), rBoostedJet_phi->at(jet_en), jet_energy);
           ///
           JesSFup = 1;
         }
-        if(BJETSF=2){      //BJet corrections with JES SF DOWN:
+        if(BJETSF==2){      //BJet corrections with JES SF DOWN:
           double jet_pt = rBoostedJet_Uncorr_pt->at(jet_en)*rBoostedJet_JesSFdown->at(jet_en);
           double jet_energy=rBoostedJet_energy->at(jet_en)*rBoostedJet_Uncorr_pt->at(jet_en)/rBoostedJet_pt->at(jet_en)*rBoostedJet_JesSFdown->at(jet_en);
           TLorentzVector JetCorr(0,0,0,0); JetCorr.SetPtEtaPhiE(jet_pt, rBoostedJet_eta->at(jet_en), rBoostedJet_phi->at(jet_en), jet_energy);
